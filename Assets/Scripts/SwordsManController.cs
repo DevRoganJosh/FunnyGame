0f6a854661673d2a29d68df8a0717c91e5fdc166
Samay28 <imsamaystar@gmail.com>
2023-05-23 11:38:21 +0530

    almost

---
+++ Assets/Scripts/SwordsManController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+public class SwordsManController : MonoBehaviour
+{
+    public PlayableDirector Time;
+    public Animator anim;
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public int maxJumpCount = 2;
+
+    private int currentJumpCount = 0;
+    private Rigidbody2D rb;
+
+    private bool isJumping = false;
+    private bool isGrounded = false;
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.state == PlayState.Paused)
+        {
+            IsRunning();
+        }
+    }
+    void IsRunning()
+    {
+        anim.SetBool("IsRunning", true);
+        isGrounded = Physics2D.OverlapCircle(transform.position, 0.4f, LayerMask.GetMask("Ground"));
+
+        // Move the character forward automatically
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+
+        // Check for a double jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (currentJumpCount < maxJumpCount)
+            {
+                // Perform the jump
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                currentJumpCount++;
+            }
+        }
+
+        // Reset the jump count when the character touches the ground
+        if (isGrounded)
+        {
+            currentJumpCount = 0;
+        }
+    }
+}
